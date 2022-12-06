5ef284ebfde725ec71e73b0d44296f499d0fd0ec
xxxx <xxxx>
2022-12-06 14:44:45 +0800

    Description:add ut
    Match-id-da060aa1607603c34423beaeb37871e9b877754a

--- interfaces/ipc/test/addwhitelistinfoscallbackunittest/add_white_list_infos_call_back_test.cpp
+++ interfaces/ipc/test/addwhitelistinfoscallbackunittest/add_white_list_infos_call_back_test.cpp
@@ -15,6 +15,8 @@
 
 #include "add_white_list_infos_call_back_test.h"
 
+#include "dinput_errcode.h"
+
 namespace OHOS {
 namespace DistributedHardware {
 namespace DistributedInput {
@@ -41,6 +43,140 @@ void AddWhiteListInfosCallbackTest::TestAddWhiteListInfosCallbackStub::OnResult(
     strJson_ = strJson;
 }
 
+void AddWhiteListInfosCallbackTest::TestGetSinkScreenInfosCallbackStub::OnResult(const std::string& strJson)
+{
+    strJson_ = strJson;
+}
+
+void AddWhiteListInfosCallbackTest::TestInputNodeListenerStub::OnNodeOnLine(const std::string srcDevId,
+    const std::string sinkDevId, const std::string sinkNodeId, const std::string sinkNodeDesc)
+{
+    srcDevId_ = srcDevId;
+    sinkDevId_ = sinkDevId;
+    sinkNodeId_ = sinkNodeId;
+    sinkNodeDesc_ = sinkNodeDesc;
+}
+
+void AddWhiteListInfosCallbackTest::TestInputNodeListenerStub::OnNodeOffLine(const std::string srcDevId,
+    const std::string sinkDevId, const std::string sinkNodeId)
+{
+    srcDevId_ = srcDevId;
+    sinkDevId_ = sinkDevId;
+    sinkNodeId_ = sinkNodeId;
+}
+
+void AddWhiteListInfosCallbackTest::TestPrepareDInputCallbackStub::OnResult(const std::string& devId,
+    const int32_t& status)
+{
+    devId_ = devId;
+    status_ = status;
+}
+
+void AddWhiteListInfosCallbackTest::TestRegisterDInputCallbackStub::OnResult(const std::string& devId,
+    const std::string& dhId, const int32_t& status)
+{
+    devId_ = devId;
+    dhId_ = dhId;
+    status_ = status;
+}
+
+int32_t AddWhiteListInfosCallbackTest::TestSharingDhIdListenerStub::OnSharing(std::string dhId)
+{
+    dhId_ = dhId;
+    return DH_SUCCESS;
+}
+
+int32_t AddWhiteListInfosCallbackTest::TestSharingDhIdListenerStub::OnNoSharing(std::string dhId)
+{
+    dhId_ = dhId;
+    return DH_SUCCESS;
+}
+
+int32_t AddWhiteListInfosCallbackTest::TestSimulationEventListenerStub::OnSimulationEvent(uint32_t type,
+    uint32_t code, int32_t value)
+{
+    type_ = type;
+    code_ = code;
+    value_ = value;
+    return DH_SUCCESS;
+}
+void AddWhiteListInfosCallbackTest::TestStartDInputCallbackStub::OnResult(const std::string& devId,
+    const uint32_t& inputTypes, const int32_t& status)
+{
+    devId_ = devId;
+    inputTypes_ = inputTypes;
+    status_ = status;
+}
+
+void AddWhiteListInfosCallbackTest::TestStartStopDInputsCallbackStub::OnResultDhids(const std::string &devId,
+    const int32_t &status)
+{
+    devId_ = devId;
+    status_ = status;
+}
+
+void AddWhiteListInfosCallbackTest::TestStartStopResultCallbackStub::OnStart(const std::string &srcId,
+    const std::string &sinkId, std::vector<std::string> &dhIds)
+{
+    srcId_ = srcId;
+    sinkId_ = sinkId;
+    dhIds.swap(dhIds);
+}
+
+void AddWhiteListInfosCallbackTest::TestStartStopResultCallbackStub::OnStop(const std::string &srcId,
+    const std::string &sinkId, std::vector<std::string> &dhIds)
+{
+    srcId_ = srcId;
+    sinkId_ = sinkId;
+    dhIds.swap(dhIds);
+}
+
+void AddWhiteListInfosCallbackTest::TestStopDInputCallbackStub::OnResult(const std::string& devId,
+    const uint32_t& inputTypes, const int32_t& status)
+{
+    devId_ = devId;
+    inputTypes_ = inputTypes;
+    status_ = status;
+}
+
+int32_t AddWhiteListInfosCallbackTest::TestDistributedInputSinkStub::Init()
+{
+    return DH_SUCCESS;
+}
+
+int32_t AddWhiteListInfosCallbackTest::TestDistributedInputSinkStub::Release()
+{
+    return DH_SUCCESS;
+}
+
+int32_t AddWhiteListInfosCallbackTest::TestDistributedInputSinkStub::RegisterGetSinkScreenInfosCallback(
+    sptr<IGetSinkScreenInfosCallback> callback)
+{
+    (void)callback;
+    return DH_SUCCESS;
+}
+
+int32_t AddWhiteListInfosCallbackTest::TestDistributedInputSinkStub::NotifyStartDScreen(
+    const SrcScreenInfo& remoteCtrlInfo)
+{
+    (void)remoteCtrlInfo;
+    return DH_SUCCESS;
+}
+
+int32_t AddWhiteListInfosCallbackTest::TestDistributedInputSinkStub::NotifyStopDScreen(
+    const std::string &srcScreenInfoKey)
+{
+    (void)srcScreenInfoKey;
+    return DH_SUCCESS;
+}
+
+int32_t AddWhiteListInfosCallbackTest::TestDistributedInputSinkStub::RegisterSharingDhIdListener(
+    sptr<ISharingDhIdListener> sharingDhIdListener)
+{
+    (void)sharingDhIdListener;
+    return DH_SUCCESS;
+}
+
 HWTEST_F(AddWhiteListInfosCallbackTest, AddWhiteListInfosCallback01, testing::ext::TestSize.Level1)
 {
     sptr<IRemoteObject> callBackStubPtr = new TestAddWhiteListInfosCallbackStub();
@@ -50,6 +186,171 @@ HWTEST_F(AddWhiteListInfosCallbackTest, AddWhiteListInfosCallback01, testing::ex
     callBackProxy.OnResult(deviceId, json);
     EXPECT_STREQ(deviceId.c_str(), ((sptr<TestAddWhiteListInfosCallbackStub> &)callBackStubPtr)->deviceId_.c_str());
     EXPECT_STREQ(json.c_str(), ((sptr<TestAddWhiteListInfosCallbackStub> &)callBackStubPtr)->strJson_.c_str());
+}
+
+HWTEST_F(AddWhiteListInfosCallbackTest, GetSinkScreenInfosCallback01, testing::ext::TestSize.Level1)
+{
+    sptr<IRemoteObject> callBackStubPtr = new TestGetSinkScreenInfosCallbackStub();
+    GetSinkScreenInfosCallbackProxy callBackProxy(callBackStubPtr);
+    std::string json = "json0";
+    callBackProxy.OnResult(json);
+    EXPECT_STREQ(json.c_str(), ((sptr<TestGetSinkScreenInfosCallbackStub> &)callBackStubPtr)->strJson_.c_str());
+}
+
+HWTEST_F(AddWhiteListInfosCallbackTest, InputNodeListenerStub01, testing::ext::TestSize.Level1)
+{
+    sptr<IRemoteObject> nodeListener = new TestInputNodeListenerStub();
+    InputNodeListenerProxy nodeListenerProxy(nodeListener);
+    std::string srcDevId = "srcDevId_test";
+    std::string sinkDevId = "sinkDevId_test";
+    std::string sinkNodeId = "sinkNodeId_test";
+    std::string sinkNodeDesc = "sinkNodeDesc_test";
+    nodeListenerProxy.OnNodeOnLine(srcDevId, sinkDevId, sinkNodeId, sinkNodeDesc);
+    EXPECT_STREQ(srcDevId.c_str(), ((sptr<TestInputNodeListenerStub> &)nodeListener)->srcDevId_.c_str());
+    EXPECT_STREQ(sinkDevId.c_str(), ((sptr<TestInputNodeListenerStub> &)nodeListener)->sinkDevId_.c_str());
+    EXPECT_STREQ(sinkNodeId.c_str(), ((sptr<TestInputNodeListenerStub> &)nodeListener)->sinkNodeId_.c_str());
+    EXPECT_STREQ(sinkNodeDesc.c_str(), ((sptr<TestInputNodeListenerStub> &)nodeListener)->sinkNodeDesc_.c_str());
+}
+
+HWTEST_F(AddWhiteListInfosCallbackTest, InputNodeListenerStub02, testing::ext::TestSize.Level1)
+{
+    sptr<IRemoteObject> nodeListener = new TestInputNodeListenerStub();
+    InputNodeListenerProxy nodeListenerProxy(nodeListener);
+    std::string srcDevId = "srcDevId_test";
+    std::string sinkDevId = "sinkDevId_test";
+    std::string sinkNodeId = "sinkNodeId_test";
+    nodeListenerProxy.OnNodeOffLine(srcDevId, sinkDevId, sinkNodeId);
+    EXPECT_STREQ(srcDevId.c_str(), ((sptr<TestInputNodeListenerStub> &)nodeListener)->srcDevId_.c_str());
+    EXPECT_STREQ(sinkDevId.c_str(), ((sptr<TestInputNodeListenerStub> &)nodeListener)->sinkDevId_.c_str());
+    EXPECT_STREQ(sinkNodeId.c_str(), ((sptr<TestInputNodeListenerStub> &)nodeListener)->sinkNodeId_.c_str());
+}
+
+HWTEST_F(AddWhiteListInfosCallbackTest, PrepareDInputCallback01, testing::ext::TestSize.Level1)
+{
+    sptr<IRemoteObject> callBackStubPtr = new TestPrepareDInputCallbackStub();
+    PrepareDInputCallbackProxy callBackProxy(callBackStubPtr);
+    std::string devId = "devId_test";
+    int32_t status = 0;
+    callBackProxy.OnResult(devId, status);
+    EXPECT_STREQ(devId.c_str(), ((sptr<TestPrepareDInputCallbackStub> &)callBackStubPtr)->devId_.c_str());
+}
+
+HWTEST_F(AddWhiteListInfosCallbackTest, RegisterDInputCallback01, testing::ext::TestSize.Level1)
+{
+    sptr<IRemoteObject> callBackStubPtr = new TestRegisterDInputCallbackStub();
+    RegisterDInputCallbackProxy callBackProxy(callBackStubPtr);
+    std::string devId = "devId_test";
+    std::string dhId = "dhId_test";
+    int32_t status = 0;
+    callBackProxy.OnResult(devId, dhId, status);
+    EXPECT_STREQ(devId.c_str(), ((sptr<TestRegisterDInputCallbackStub> &)callBackStubPtr)->devId_.c_str());
+    EXPECT_STREQ(dhId.c_str(), ((sptr<TestRegisterDInputCallbackStub> &)callBackStubPtr)->dhId_.c_str());
+}
+
+HWTEST_F(AddWhiteListInfosCallbackTest, SharingDhIdListenerStub01, testing::ext::TestSize.Level1)
+{
+    sptr<IRemoteObject> sharingStubPtr = new TestSharingDhIdListenerStub();
+    SharingDhIdListenerProxy sharingProxy(sharingStubPtr);
+    std::string dhId = "dhId_test";
+    int32_t ret = sharingProxy.OnSharing(dhId);
+    EXPECT_EQ(DH_SUCCESS, ret);
+}
+
+HWTEST_F(AddWhiteListInfosCallbackTest, SharingDhIdListenerStub02, testing::ext::TestSize.Level1)
+{
+    sptr<IRemoteObject> sharingStubPtr = new TestSharingDhIdListenerStub();
+    SharingDhIdListenerProxy sharingProxy(sharingStubPtr);
+    std::string dhId = "dhId_test";
+    int32_t ret = sharingProxy.OnNoSharing(dhId);
+    EXPECT_EQ(DH_SUCCESS, ret);
+}
+
+HWTEST_F(AddWhiteListInfosCallbackTest, SimulationEventListenerStub01, testing::ext::TestSize.Level1)
+{
+    sptr<IRemoteObject> simulationStubPtr = new TestSimulationEventListenerStub();
+    SimulationEventListenerProxy sharingProxy(simulationStubPtr);
+    uint32_t type = 1;
+    uint32_t code = 1;
+    int32_t value = 1;
+    int32_t ret = sharingProxy.OnSimulationEvent(type, code, value);
+    EXPECT_EQ(DH_SUCCESS, ret);
+}
+
+HWTEST_F(AddWhiteListInfosCallbackTest, StartDInputCallbackStub01, testing::ext::TestSize.Level1)
+{
+    sptr<IRemoteObject> callBackStubPtr = new TestStartDInputCallbackStub();
+    StartDInputCallbackProxy callBackProxy(callBackStubPtr);
+    std::string devId = "devId_test";
+    uint32_t inputTypes = static_cast<uint32_t>(DInputDeviceType::ALL);
+    int32_t status = 1;
+    callBackProxy.OnResult(devId, inputTypes, status);
+    EXPECT_STREQ(devId.c_str(), ((sptr<TestStartDInputCallbackStub> &)callBackStubPtr)->devId_.c_str());
+}
+
+HWTEST_F(AddWhiteListInfosCallbackTest, StartStopDInputsCallbackStub01, testing::ext::TestSize.Level1)
+{
+    sptr<IRemoteObject> callBackStubPtr = new TestStartStopDInputsCallbackStub();
+    StartStopDInputsCallbackProxy callBackProxy(callBackStubPtr);
+    std::string devId = "devId_test";
+    int32_t status = 1;
+    callBackProxy.OnResultDhids(devId, status);
+    EXPECT_STREQ(devId.c_str(), ((sptr<TestStartStopDInputsCallbackStub> &)callBackStubPtr)->devId_.c_str());
+    EXPECT_EQ(status, ((sptr<TestStartStopDInputsCallbackStub> &)callBackStubPtr)->status_);
+}
+
+HWTEST_F(AddWhiteListInfosCallbackTest, StartStopResultCallbackStub01, testing::ext::TestSize.Level1)
+{
+    sptr<IRemoteObject> callBackStubPtr = new TestStartStopResultCallbackStub();
+    StartStopResultCallbackProxy callBackProxy(callBackStubPtr);
+    std::string srcId = "srcId_test";
+    std::string sinkId = "sinkId_test";
+    std::vector<std::string> dhIds;
+    dhIds.push_back("dhId_test");
+    callBackProxy.OnStart(srcId, sinkId, dhIds);
+    EXPECT_STREQ(srcId.c_str(), ((sptr<TestStartStopResultCallbackStub> &)callBackStubPtr)->srcId_.c_str());
+    EXPECT_STREQ(sinkId.c_str(), ((sptr<TestStartStopResultCallbackStub> &)callBackStubPtr)->sinkId_.c_str());
+}
+
+HWTEST_F(AddWhiteListInfosCallbackTest, StopDInputCallbackStub01, testing::ext::TestSize.Level1)
+{
+    sptr<IRemoteObject> callBackStubPtr = new TestStopDInputCallbackStub();
+    StopDInputCallbackProxy callBackProxy(callBackStubPtr);
+    std::string devId = "devId_test";
+    uint32_t inputTypes = static_cast<uint32_t>(DInputDeviceType::ALL);
+    int32_t status = 1;
+    callBackProxy.OnResult(devId, inputTypes, status);
+    EXPECT_STREQ(devId.c_str(), ((sptr<TestStopDInputCallbackStub> &)callBackStubPtr)->devId_.c_str());
+}
+
+HWTEST_F(AddWhiteListInfosCallbackTest, DistributedInputSinkStub01, testing::ext::TestSize.Level1)
+{
+    sptr<IRemoteObject> sinkStubPtr = new TestDistributedInputSinkStub();
+    DistributedInputSinkProxy sinkProxy(sinkStubPtr);
+    int32_t ret = sinkProxy.Init();
+    EXPECT_EQ(DH_SUCCESS, ret);
+
+    ret = sinkProxy.Release();
+    EXPECT_EQ(DH_SUCCESS, ret);
+
+    sptr<IGetSinkScreenInfosCallback> callback = nullptr;
+    ret = sinkProxy.RegisterGetSinkScreenInfosCallback(callback);
+    EXPECT_EQ(ERR_DH_INPUT_IPC_WRITE_TOKEN_VALID_FAIL, ret);
+
+    sptr<IGetSinkScreenInfosCallback> sinkScreenCb = new TestGetSinkScreenInfosCallbackStub();
+    ret = sinkProxy.RegisterGetSinkScreenInfosCallback(sinkScreenCb);
+    EXPECT_EQ(DH_SUCCESS, ret);
+
+    SrcScreenInfo srcScreenInfo {"devid_test", "uuid_test", 1, 1, 1860, 980, "srcphyid", 1, 980, 490};
+    ret = sinkProxy.NotifyStartDScreen(srcScreenInfo);
+    EXPECT_EQ(DH_SUCCESS, ret);
+
+    std::string srcScreenInfoKey = "srcScreenInfoKey_test";
+    ret = sinkProxy.NotifyStopDScreen(srcScreenInfoKey);
+    EXPECT_EQ(DH_SUCCESS, ret);
+
+    sptr<ISharingDhIdListener> sharingDhIdListene = new TestSharingDhIdListenerStub();
+    ret = sinkProxy.RegisterSharingDhIdListener(sharingDhIdListene);
+    EXPECT_EQ(DH_SUCCESS, ret);
 }
 } // namespace DistributedInput
 } // namespace DistributedHardware
--- interfaces/ipc/test/addwhitelistinfoscallbackunittest/add_white_list_infos_call_back_test.h
+++ interfaces/ipc/test/addwhitelistinfoscallbackunittest/add_white_list_infos_call_back_test.h
@@ -20,6 +20,28 @@
 
 #include "add_white_list_infos_call_back_proxy.h"
 #include "add_white_list_infos_call_back_stub.h"
+#include "get_sink_screen_infos_call_back_proxy.h"
+#include "get_sink_screen_infos_call_back_stub.h"
+#include "distributed_input_sink_proxy.h"
+#include "distributed_input_sink_stub.h"
+#include "input_node_listener_proxy.h"
+#include "input_node_listener_stub.h"
+#include "prepare_d_input_call_back_proxy.h"
+#include "prepare_d_input_call_back_stub.h"
+#include "sharing_dhid_listener_proxy.h"
+#include "sharing_dhid_listener_stub.h"
+#include "simulation_event_listener_proxy.h"
+#include "simulation_event_listener_stub.h"
+#include "start_d_input_call_back_proxy.h"
+#include "start_d_input_call_back_stub.h"
+#include "start_stop_d_inputs_call_back_proxy.h"
+#include "start_stop_d_inputs_call_back_stub.h"
+#include "start_stop_result_call_back_proxy.h"
+#include "start_stop_result_call_back_stub.h"
+#include "stop_d_input_call_back_proxy.h"
+#include "stop_d_input_call_back_stub.h"
+#include "register_d_input_call_back_proxy.h"
+#include "register_d_input_call_back_stub.h"
 
 namespace OHOS {
 namespace DistributedHardware {
@@ -39,6 +61,130 @@ public:
         void OnResult(const std::string& deviceId, const std::string& strJson);
         std::string deviceId_;
         std::string strJson_;
+    };
+
+    class TestGetSinkScreenInfosCallbackStub : public
+        OHOS::DistributedHardware::DistributedInput::GetSinkScreenInfosCallbackStub {
+    public:
+        TestGetSinkScreenInfosCallbackStub() = default;
+        virtual ~TestGetSinkScreenInfosCallbackStub() = default;
+        void OnResult(const std::string& strJson);
+        std::string strJson_;
+    };
+
+    class TestInputNodeListenerStub : public
+        OHOS::DistributedHardware::DistributedInput::InputNodeListenerStub {
+    public:
+        TestInputNodeListenerStub() = default;
+        virtual ~TestInputNodeListenerStub() = default;
+        void OnNodeOnLine(const std::string srcDevId, const std::string sinkDevId,
+            const std::string sinkNodeId, const std::string sinkNodeDesc);
+
+        void OnNodeOffLine(const std::string srcDevId, const std::string sinkDevId,
+            const std::string sinkNodeId);
+        std::string srcDevId_;
+        std::string sinkDevId_;
+        std::string sinkNodeId_;
+        std::string sinkNodeDesc_;
+    };
+
+    class TestPrepareDInputCallbackStub : public
+        OHOS::DistributedHardware::DistributedInput::PrepareDInputCallbackStub {
+    public:
+        TestPrepareDInputCallbackStub() = default;
+        virtual ~TestPrepareDInputCallbackStub() = default;
+        void OnResult(const std::string& devId, const int32_t& status);
+        std::string devId_;
+        int32_t status_ = 0;
+    };
+
+    class TestRegisterDInputCallbackStub : public
+        OHOS::DistributedHardware::DistributedInput::RegisterDInputCallbackStub {
+    public:
+        TestRegisterDInputCallbackStub() = default;
+        virtual ~TestRegisterDInputCallbackStub() = default;
+        void OnResult(const std::string& devId, const std::string& dhId, const int32_t& status);
+        std::string devId_;
+        std::string dhId_;
+        int32_t status_ = 0;
+    };
+
+    class TestSharingDhIdListenerStub : public
+        OHOS::DistributedHardware::DistributedInput::SharingDhIdListenerStub {
+    public:
+        TestSharingDhIdListenerStub() = default;
+        virtual ~TestSharingDhIdListenerStub() = default;
+        int32_t OnSharing(std::string dhId);
+        int32_t OnNoSharing(std::string dhId);
+        std::string dhId_;
+    };
+
+    class TestSimulationEventListenerStub : public
+        OHOS::DistributedHardware::DistributedInput::SimulationEventListenerStub {
+    public:
+        TestSimulationEventListenerStub() = default;
+        virtual ~TestSimulationEventListenerStub() = default;
+        int32_t OnSimulationEvent(uint32_t type, uint32_t code, int32_t value);
+        uint32_t type_ = 0;
+        uint32_t code_ = 0;
+        int32_t value_ = 0;
+    };
+
+    class TestStartDInputCallbackStub : public
+        OHOS::DistributedHardware::DistributedInput::StartDInputCallbackStub {
+    public:
+        TestStartDInputCallbackStub() = default;
+        virtual ~TestStartDInputCallbackStub() = default;
+        void OnResult(const std::string& devId, const uint32_t& inputTypes, const int32_t& status);
+        std::string devId_;
+        uint32_t inputTypes_ = 0;
+        int32_t status_ = 0;
+    };
+
+    class TestStartStopDInputsCallbackStub : public
+        OHOS::DistributedHardware::DistributedInput::StartStopDInputsCallbackStub {
+    public:
+        TestStartStopDInputsCallbackStub() = default;
+        virtual ~TestStartStopDInputsCallbackStub() = default;
+        void OnResultDhids(const std::string &devId, const int32_t &status);
+        std::string devId_;
+        int32_t status_ = 0;
+    };
+
+    class TestStartStopResultCallbackStub : public
+        OHOS::DistributedHardware::DistributedInput::StartStopResultCallbackStub {
+    public:
+        TestStartStopResultCallbackStub() = default;
+        virtual ~TestStartStopResultCallbackStub() = default;
+        void OnStart(const std::string &srcId, const std::string &sinkId, std::vector<std::string> &dhIds);
+        void OnStop(const std::string &srcId, const std::string &sinkId, std::vector<std::string> &dhIds);
+        std::string srcId_;
+        std::string sinkId_;
+        std::vector<std::string> dhIds_;
+    };
+
+    class TestStopDInputCallbackStub : public
+        OHOS::DistributedHardware::DistributedInput::StopDInputCallbackStub {
+    public:
+        TestStopDInputCallbackStub() = default;
+        virtual ~TestStopDInputCallbackStub() = default;
+        void OnResult(const std::string& devId, const uint32_t& inputTypes, const int32_t& status);
+        std::string devId_;
+        uint32_t inputTypes_ = 0;
+        int32_t status_ = 0;
+    };
+
+    class TestDistributedInputSinkStub : public
+        OHOS::DistributedHardware::DistributedInput::DistributedInputSinkStub {
+    public:
+        TestDistributedInputSinkStub() = default;
+        virtual ~TestDistributedInputSinkStub() = default;
+        int32_t Init();
+        int32_t Release();
+        int32_t RegisterGetSinkScreenInfosCallback(sptr<IGetSinkScreenInfosCallback> callback);
+        int32_t NotifyStartDScreen(const SrcScreenInfo& remoteCtrlInfo);
+        int32_t NotifyStopDScreen(const std::string &srcScreenInfoKey);
+        int32_t RegisterSharingDhIdListener(sptr<ISharingDhIdListener> sharingDhIdListener);
     };
 };
 } // namespace DistributedInput
